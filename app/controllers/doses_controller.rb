21c93087b37a3a8cca11f74486ed397c41e244ee
Roger Tosbotn <rogertosb@gmail.com>
2020-02-20 16:16:13 +0100

    aaaalmost errything workin

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,31 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(doses_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_doses
+  end
+
+  def doses_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
